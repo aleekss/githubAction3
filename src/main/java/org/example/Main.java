8f710868386cadb84dfc5a58fd2f1477cea81c8e
adilic2319 <adilic@htl-steyr.ac.at>
2023-11-23 13:01:04 +0100

    Class added for Rectangle-Area

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt());
+        Rechteck rechteck = new Rechteck(5,3);
 
+        rechteck.berechneFlaeche();
     }
 }
+
+
